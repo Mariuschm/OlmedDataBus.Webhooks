9b8f60a8174f66ad8f7ce5e0507ece3a0d804412
Mariusz Midzio <mariusz.midzio@prospeo.com.pl>
2026-08-10 13:00:59 +0200

    Przygotowana procedura pod procesy do wysyłki do KSeF i do CSM

---
+++ Prospeo.DbContext/SQL_SCRIPTS/Granty.sql
@@ -0,0 +1,12 @@
+﻿/*
+    Poprawne zmiany:
+    1) Ujednolicono formatowanie i wielkość słów kluczowych SQL.
+    2) Dodano średniki kończące instrukcje.
+    3) Ujednolicono zapisy nazw schematu, tabeli i użytkowników.
+*/
+
+GRANT INSERT, UPDATE, SELECT ON SCHEMA::[ProRws] TO [CDNStdADOOlmed];
+GRANT INSERT, UPDATE, SELECT ON SCHEMA::[ProRws] TO [CDNStdOlmed];
+
+GRANT INSERT, UPDATE, SELECT ON [ProRws].[Queue] TO [CDNStdOlmed];
+GRANT INSERT, UPDATE, SELECT ON [ProRws].[Queue] TO [CDNStdADOOlmed];
